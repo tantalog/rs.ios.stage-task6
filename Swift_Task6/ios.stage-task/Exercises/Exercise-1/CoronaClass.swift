093746316e6cf457fc6fc94f3b65b31aaa13c61a
tantalog <kushnerovanton@gmail.com>
2021-06-21 20:00:27 +0300

    feat: exercise 1 complete

--- Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
+++ Swift_Task6/ios.stage-task/Exercises/Exercise-1/CoronaClass.swift
@@ -1,18 +1,65 @@
 import Foundation
 
 class CoronaClass {
- 
-     var seats = [Int]()
-
-     init(n: Int) {
- 
-     }
-     
-     func seat() -> Int {
-        -1
-     }
-     
-     func leave(_ p: Int) {
-         
-     }
+    
+    var seats = [Int]()
+    var n: Int
+    
+    init(n: Int) {
+        self.n = n
+    }
+    
+    func seat() -> Int {
+        
+        var minSeats = 0
+        var maxSeats = -1
+        var variantSeat = 0
+        
+        if seats.isEmpty {
+            seats.append(0)
+            return minSeats
+        }
+        
+        
+        for item in 0..<seats.count - 1{
+            
+            variantSeat = (seats[item] + seats[item + 1]) / 2
+            
+            var tempMin = 0
+            if variantSeat - seats[item] <= seats[item + 1] - variantSeat{
+                tempMin = variantSeat - seats[item]
+            }else{
+                tempMin = seats[item + 1] - variantSeat
+            }
+            
+            if tempMin > maxSeats{
+                maxSeats = tempMin
+                print(maxSeats)
+                minSeats = variantSeat
+                
+            }
+            
+        }
+        
+        
+        let tempVal = (n - 2) - seats[seats.count - 1]
+        
+        if tempVal  > maxSeats{
+            maxSeats = tempVal
+            minSeats = n - 1
+            
+        }
+        
+        seats.append(minSeats)
+        seats.sort(by: <)
+        
+        return minSeats
+    }
+    
+    
+    func leave(_ p: Int) {
+        
+        guard let i = seats.firstIndex(of: p)  else { return}
+        seats.remove(at: i)
+    }
 }
